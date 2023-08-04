05e6ad66365a88ff8983f16a656f18aa29dbce7f
kkendirci <kubrakendirci0@gmail.com>
2023-08-04 13:31:22 +0300

    TimeConversition question solutions

---
+++ app/src/main/java/com/example/hackerranksolutions/preparationkit/day1/TimeConversion.kt
@@ -0,0 +1,36 @@
+package com.example.hackerranksolutions.preparationkit.day1
+/*
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+*/
+fun timeConversion(s: String): String {
+    val regex = Regex("(\\d\\d):(\\d\\d):(\\d\\d)(AM|PM)")
+    val result = regex.find(s)!!
+    val (hour, minute, second, amorpm) = result.destructured
+
+    var h = hour.toInt(10)
+
+    if (amorpm == "PM") {
+        if (h != 12) {
+            h += 12
+        }
+    } else {
+        if (h == 12) {
+            h = 0
+        }
+    }
+
+    return "%02d:%s:%s".format(h,minute,second)
+
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val result = timeConversion(s)
+
+    println(result)
+}
