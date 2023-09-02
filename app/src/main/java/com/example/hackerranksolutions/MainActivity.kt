09b0201297342bef662bec2dc8f75da27b77c997
kkendirci <kubrakendirci0@gmail.com>
2023-09-02 20:21:52 +0300

    lonelyInteger question solutions

--- app/src/main/java/com/example/hackerranksolutions/MainActivity.kt
+++ app/src/main/java/com/example/hackerranksolutions/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.hackerranksolutions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackerranksolutions.preparationkit.day2.lonelyinteger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val arr=arrayOf(1,2,3,4,3,2,1)
+        val result= lonelyinteger(arr)
+        println(result)
     }
 }
